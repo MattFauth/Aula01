20da36d078bc73d19940c83cd0771379e6f02da6
MattFauth <41974587+MattFauth@users.noreply.github.com>
2019-12-09 23:45:38 -0300

    Update forms.py

--- Trabalho 2 de G1/Trabalho2G1/app_processo/forms.py
+++ Trabalho 2 de G1/Trabalho2G1/app_processo/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContatoForm(forms.Form):
-    nome = forms.CharField(max_length=127, min_length=12)
+    nome = forms.CharField(max_length=128, min_length=12)
     email = forms.EmailField(required=False)
     mensagem = forms.CharField(widget=forms.Textarea)
 
@@ -10,7 +10,7 @@ class ContatoForm(forms.Form):
         dados = super().clean()
         email = dados.get('email', None)
         mensagem = dados.get('mensagem')
-        
+
         if '@hotmail.com' in email:
             self.add_error('email', 'Provedor de e-mail nao suportado (hotmail.com)')
         palavras = ['porra', 'problema', 'defeito', 'erro', 'comunismo']
